d9852a173bb5161139255d65d2a46383533b0ef9
Chekube <mdankaro@gmail.com>
2021-06-25 04:10:24 +0100

    faq

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => (
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -Bring in toggle buttons (querySelectorAll)
+// -Loop through nodeList (forEach)
+// -Addclick event (addEventListener)
+// Toggle the active class on the parent node (.parentNode & classList.toggle())
